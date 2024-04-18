02396b9f6559ba0fa10d225b73bfa78ae2a59a18
Maty Q <matiasequintas@gmail.com>
2024-04-17 21:02:17 -0300

    Sumador v3.0

--- sumador.c
+++ sumador.c
@@ -4,14 +4,16 @@
 
 int main(void)
 {
-    int num1, num2, num3, suma;
+    int num1, num2, num3, num4, suma;
     p("Ingrese el primer valor: ");
     s("%d", &num1);
     p("Ingrese el segundo valor: ");
     s("%d", &num2);
     p("Ingrese el tercer valor: ");
     s("%d", &num3);
-    suma = num1 + num2;
+    p("Ingrese el cuarto valor: ");
+    s("%d", &num4);
+    suma = num1 + num2 + num3 + num4;
     p("%d + %d + %d = %d", num1, num2, num3, suma);
     return 0;
 }
